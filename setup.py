8e298b51637729900e61fb94e917cd02d145a5b8
phlummox <phlummox@users.noreply.github.com>
2022-03-22 18:50:39 +0800

    remove py-pandoc

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     packages=find_packages(exclude=['docs', 'tests']),
 
     install_requires=['jupyter_core', 'traitlets', 'ipython', 'jupyter_client', 'ipykernel',
-                      'nbconvert>=5.4.1', 'pandocfilters', 'py-pandoc>=2.6',
+                      'nbconvert>=5.4.1', 'pandocfilters',
                       'click', 'psutil', 'panflute>=1.11.2', 'shutilwhich-cwdpatch>=0.1.0',
                       'pyyaml'],
     # jupyter_core traitlets ipython jupyter_client nbconvert pandocfilters "py-pandoc>=2.6" click psutil "panflute>=1.11.2" pyyaml "shutilwhich-cwdpatch>=0.1.0" ipykernel
